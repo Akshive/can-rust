51d50e78ed3fa5c6ee2d1ed85ae52abd1c004c27
Akshive <pandeyakshive97@gmail.com>
2023-10-28 04:43:57 +0530

    Adding cache

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, collections::HashMap};
 
-use axum::{response::IntoResponse, routing::get, Router, http::{Request, HeaderMap}};
+use axum::{response::IntoResponse, Router, http::{HeaderMap, Uri}};
 use miette::{Result, IntoDiagnostic};
 
 const PROXY_FROM_DOMAIN: &str = "slow-server.akshive.test";
@@ -25,14 +25,13 @@ async fn main () -> Result<()>
     Ok(())
 }
 
-async fn proxy_request<Body>(
+async fn proxy_request(
+    uri: Uri,
     host: axum::extract::Host, 
     headers: HeaderMap,
     method: axum::http::Method,
-    request: Request<Body>) -> Result<impl IntoResponse, String>
-{
-    let uri = request.uri();
-    
+    bytes: axum::body::Bytes) -> Result<impl IntoResponse, String>
+{  
     let split = host.0.split(":").collect::<Vec<_>>();
     let host_name = split[0];
 
@@ -43,20 +42,68 @@ async fn proxy_request<Body>(
 
     let path = uri.path_and_query().map(|pq| pq.path()).unwrap_or("/");
 
+    let url_builder = axum::http::Uri::builder().scheme("http").authority(PROXY_ORIGIN_DOMAIN);
+    let url = url_builder.path_and_query(path).build().map_err(|_| "Could not build url")?;
+
+    let response = try_get_cached_response(method, url, headers, bytes).await?;
+
+    Ok((
+        response.status(),
+        response.headers().clone(),
+        response.into_body()
+    ))
+}
+
+type CacheKey = (axum::http::Method, axum::http::Uri);
+
+lazy_static::lazy_static! 
+{
+    static ref CACHE: std::sync::Mutex<HashMap<CacheKey, axum::http::Response<axum::body::Bytes>>>  = std::sync::Mutex::new(HashMap::new());
+}
+
+async fn try_get_cached_response(
+    method: reqwest::Method,
+    url: axum::http::Uri,
+    headers: HeaderMap,
+    body: axum::body::Bytes
+) -> Result<axum::http::Response<axum::body::Bytes>, String>
+{
+    {
+        let cache = CACHE.lock().unwrap();
+        let cached_response = cache.get(&(method.clone(), url.clone()));
+
+        if let Some(cached) = cached_response 
+        {
+            let mut response = axum::http::Response::builder().status(cached.status());
+            for (key, value) in cached.headers().iter() 
+            {
+                response = response.header(key, value)
+            }
+            let response = response.body(cached.body().clone()).map_err(|_| "Could not build response")?;
+
+            return Ok(response);
+        }
+    }
+
     let client = reqwest::Client::new();
 
-    dbg!(&method, &path);
-    
-    let response = client
-    .request(method, format!("https://{PROXY_ORIGIN_DOMAIN}{path}"))
+    let origin_response = client
+    .request(method, url.to_string())
+    .body(body)
     .headers(headers)
     .send()
     .await
     .map_err(|_| "Request failed")?;
 
-    Ok((
-        response.status(),
-        response.headers().clone(),
-        response.bytes().await.into_diagnostic().map_err(|_| "Could not get bytes from header")?,
-    ))
+    let mut response = axum::http::Response::builder().status(origin_response.status());
+    for (key, value) in origin_response.headers().iter() 
+    {
+        response = response.header(key, value)
+    }
+    let response = response.body(
+        origin_response.bytes().await.map_err(|_| "Could not get bytes from origin response")?
+    )
+    .map_err(|_| "Could not build response")?;
+    
+    Ok(response)
 }
